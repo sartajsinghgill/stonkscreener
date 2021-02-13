4f359cd3c5cb6edc0c0047d39fe07f373ed9c686
Sartaj <sartaj@atomicsquare.com>
2021-02-13 15:16:41 -0500

    Added comments and renamed the Get price button to show Find Stonk instead

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+# Default route will render index.html that is located in the templates/ folder
 @app.route('/')
 def index():
     return render_template("index.html")
 
+# /stonks/symbol route will take the symbol that is passed to the route and use that as a query parameter for the yfinance API and return the stock information
 @app.route('/stonks/<symbol>')
 def getStonk(symbol):
     stonkName = yf.Ticker(symbol)
