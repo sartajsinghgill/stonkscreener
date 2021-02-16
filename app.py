f7d1697e33414589b0c117b14f19f3a35845fa65
Sartaj <sartaj@atomicsquare.com>
2021-02-15 23:30:19 -0500

    Adding a stonk history function in flask

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import yfinance as yf
+import json
+
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
@@ -11,5 +13,13 @@ def index():
 # /stonks/symbol route will take the symbol that is passed to the route and use that as a query parameter for the yfinance API and return the stock information
 @app.route('/stonks/<symbol>')
 def getStonk(symbol):
+    return(getStockDataFromYF(symbol).info)
+
+@app.route('/stonks/<symbol>/history')
+def getStonkHistory(symbol):
+    json_data = json.loads(getStockDataFromYF(symbol).history().to_json(orient="records"))
+    return json_data[0]
+
+def getStockDataFromYF(symbol):
     stonkName = yf.Ticker(symbol)
-    return(stonkName.info)
+    return stonkName
